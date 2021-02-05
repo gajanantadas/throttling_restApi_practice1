f615965ea966e14cd0055c0970a25df074c2b80c
gajanan <gajanantadas3@gmail.com>
2021-02-05 19:08:07 +0530

    Initial commit

---
+++ throttling_restApi_practice1/urls.py
@@ -0,0 +1,11 @@
+from rest_framework.routers import DefaultRouter
+from django.contrib import admin
+from django.urls import path,include
+from api import views
+router=DefaultRouter()
+router.register("",views.StudentApi,basename="student")
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('',include(router.urls)),
+    path("auth/",include("rest_framework.urls",namespace='rest_framework'))
+]
